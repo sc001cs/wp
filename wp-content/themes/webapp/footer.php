38e33e52e23a4c735bcfa89d19fe858fdc07c9c5
bb <arditjuventus@hotmail.com>
2016-08-27 19:41:08 +0200

    Setting up the theme III

---
+++ wp-content/themes/webapp/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WEBAPP
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'webapp' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'webapp' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'webapp' ), 'webapp', '<a href="http://webapp.al" rel="designer">webapp.al</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
